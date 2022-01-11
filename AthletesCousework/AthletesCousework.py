ee9d35148d5006c18586c966f9614f7ea422cf06
Adelekan Oba <u2192659@live.warwick.ac.uk>
2022-01-11 18:32:08 +0000

    Functional Requirement 1 fulfilled. Code for functional requirement 2 has not been started.

--- AthletesCousework/AthletesCousework.py
+++ AthletesCousework/AthletesCousework.py
@@ -1,20 +1,50 @@
 import csv
 
 class Athletes:
+    
 
 
-    def openAthletes(self):
+
+    #def openAthletes(self):
         
-        with open('Athletes.csv', 'r',) as csv_file:
-            csv_reader = csv.reader(csv_file)
-            for row in csv_reader:
-                print(row)
+        #with open('Athletes.csv', 'r',) as file:
+            #reader = csv.reader(file)
+            #for row in reader:
+             #   nameColumn.append(row[0])
+            #for item in nameColumn:
+             #   print(item)
+
+    #def openAthletes2(self):
+       # filename = open('Athletes.csv', 'r')
+        #file = csv.DictReader(filename)
 
 
-           
+    def listByLength(self):
+        
+        dictAthletes = dict() #Create dictionary to store CSV sections
+        for column in file:
+            key = column["Name"]
+            value = {"NOC": column["NOC"], "Discipline": column["Discipline"]}
+            dictAthletes[key]= value
+
+            #Iterate through the CSV file and set the column Name as the key, and the NOC and Discipline as the values.
+
+        
+        #Method to sort names in order of length
+        #sortedNames = sorted(key, key=lambda x: -len(x), reverse=True) #Sort the names list in order of length
+        #print(*sortedNames, sep = '\n') #Change the format of the list from vertical to horizontal
+
+        orderedDict = {}
+        for k in sorted(dictAthletes, key = len):
+           orderedDict[k] = dictAthletes[k]
+           print(k, orderedDict[k])
+
+ 
+        
 
 
 
-
-Adelekan = Athletes()
-Adelekan.openAthletes()
+Adelekan = Athletes() #Create an object of class athletes
+AthletesList = open('Athletes.csv', 'r') #Open Athletes.csv in read mode
+file = csv.DictReader(AthletesList) #Create an object containing the content in the csv file
+Adelekan.listByLength()                                                        
